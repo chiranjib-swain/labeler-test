86b3427b123b6401c16e0cbc4689515475b06cd8
chiranjib-swain <chiranjib-swain@github.com>
2025-05-29 12:11:55 +0530

    changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 // A sample component
 function Button({ label }) {
-    return `<button>${label}</button>`;// thsis is a simple button component test for 870
+    return `<button>${label}</button>`;// thsis is a simple button component test for 870 .
   }
   module.exports = Button;
