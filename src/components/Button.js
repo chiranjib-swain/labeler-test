88406da83df1d764903dc47a4517fcc31550d8f9
chiranjib-swain <chiranjib-swain@github.com>
2025-05-29 15:31:04 +0530

    changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 // A sample component
 function Button({ label }) {
-    return `<button>${label}</button>`;// thsis is a simple button component test for 870 . issues added hdfggdsdsd
+    return `<button>${label}</button>`;// thsis is a simple button component test for 870 . issues added hdfggdsdsdgfdgf
   }
   module.exports = Button;
