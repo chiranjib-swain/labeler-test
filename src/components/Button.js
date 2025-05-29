9d4c4dca1ee8be32c68e43f54ce390151626dc7b
chiranjib-swain <chiranjib-swain@github.com>
2025-05-29 15:24:08 +0530

    github token removed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 // A sample component
 function Button({ label }) {
-    return `<button>${label}</button>`;// thsis is a simple button component test for 870 . issues added
+    return `<button>${label}</button>`;// thsis is a simple button component test for 870 . issues added hdfggd
   }
   module.exports = Button;
